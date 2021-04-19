22f8201d4845666e69635975c2d27804483465cb
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-04-19 15:33:35 -0500

    update checks

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from mysqlconnection import connectToMySQL
 import re	#the regex module
 #this validates our email, MATCHES PATTERN
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+#this validates the names
+NAME_REGEX = re.compile(r'^[[a-zA-Z]+$')
 
 app = Flask(__name__)
 app.secret_key = "speak friend and enter"
@@ -36,6 +38,14 @@ def register_user():
         # test whether a field matches the pattern
         flash("Invalid email address!")
         is_valid = False
+    if not NAME_REGEX.match(request.form['first_name']):
+        # test whether a field matches the pattern
+        flash("Invalid first name!")
+        is_valid = False
+    if not NAME_REGEX.match(request.form['last_name']):
+        # test whether a field matches the pattern
+        flash("Invalid last name!")
+        is_valid = False
 #FLASH VALIDATION
 # make sure you add the FLASH MESSAGE LOOPS to your HTML!!! you keep forgetting!!
     if len(request.form['first_name']) < 1:
@@ -50,27 +60,28 @@ def register_user():
     if request.form['password'] != request.form['password_confirm']:
         is_valid = False
         flash("Your passwords did not match")
-    # see if the username provided exists in the database
-    # query the db then check
+# see if the username/email provided exists in the database already
+# query the db then check
     mysql = connectToMySQL("login_reg_schema")
-    query = "SELECT * FROM users WHERE email = %(email)s;"
-    data = { "email" : request.form["email"] }
-    email_check = mysql.query_db(query, data)
-    email_in_db = email_check[0]['email']
-    # check the email
-    if request.form['email'] == email_in_db:
-        is_valid = False
-        flash("Email already registered")
+    query = "SELECT * FROM users;"
+    data = { }
+    user_table = mysql.query_db(query, data)
+#use a for loop to loop through the table
+    for row in user_table:
+        for i in row:
+            if row['email'] == request.form['email']:
+                is_valid = False
+                flash("Email already taken")
+                break
 #IF VALID, CONNECT TO DB
     if is_valid:
-        # FIRST CALL THE DB
+        #FIRST CALL THE DB
         mysql = connectToMySQL('login_reg_schema')
         #HASH THE PASSWORD
         pw_hash = bcrypt.generate_password_hash(request.form['password'])  
         print(pw_hash)
         #THEN QUERY THE DB
         query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW());"
-
         data = {
             "first_name": request.form["first_name"],
             "last_name": request.form["last_name"],
@@ -109,6 +120,12 @@ def login_user():
 
     flash("Invalid email or password, please try again")
     return redirect("/")
+
+@app.route('/log_out', methods=["POST"])
+def log_out():
+    session.clear()
+    return redirect("/")
+#VALIDATION
 
 if __name__ == "__main__":
     app.run(debug=True)

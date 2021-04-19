28510aac12138dbd8b9cda4ea4fe3d0fbcfb24fe
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-04-19 13:45:49 -0500

    init

---
+++ server.py
@@ -0,0 +1,114 @@
+from flask import Flask, render_template, request, redirect, session, flash
+from mysqlconnection import connectToMySQL
+
+import re	#the regex module
+#this validates our email, MATCHES PATTERN
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+app = Flask(__name__)
+app.secret_key = "speak friend and enter"
+#need secret key for session
+
+#imports b-crypt
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
+# this needs to be after you declare app or it doesn't recognize
+
+#RUN THIS TO LAUNCH python -m pipenv install flask PyMySQL
+#AFTER YOU ACTIVATE RUN THIS to install bcrpt python -m pipenv install flask-bcrypt
+
+#LANDING PAGES
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+#PROCESSES
+@app.route('/register_user', methods=["POST"])
+def register_user():
+#VALIDATION
+    is_valid = True
+#REGEX VALIDATION
+    if not EMAIL_REGEX.match(request.form['email']):
+        # test whether a field matches the pattern
+        flash("Invalid email address!")
+        is_valid = False
+#FLASH VALIDATION
+# make sure you add the FLASH MESSAGE LOOPS to your HTML!!! you keep forgetting!!
+    if len(request.form['first_name']) < 1:
+        is_valid = False
+        flash("Please enter a first name")
+    if len(request.form['last_name']) < 1:
+        is_valid = False
+        flash("Please enter a last name")
+    if len(request.form['password']) < 8:
+        is_valid = False
+        flash("Password must be at least 8 characters")
+    if request.form['password'] != request.form['password_confirm']:
+        is_valid = False
+        flash("Your passwords did not match")
+    # see if the username provided exists in the database
+    # query the db then check
+    mysql = connectToMySQL("login_reg_schema")
+    query = "SELECT * FROM users WHERE email = %(email)s;"
+    data = { "email" : request.form["email"] }
+    email_check = mysql.query_db(query, data)
+    email_in_db = email_check[0]['email']
+    # check the email
+    if request.form['email'] == email_in_db:
+        is_valid = False
+        flash("Email already registered")
+#IF VALID, CONNECT TO DB
+    if is_valid:
+        # FIRST CALL THE DB
+        mysql = connectToMySQL('login_reg_schema')
+        #HASH THE PASSWORD
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])  
+        print(pw_hash)
+        #THEN QUERY THE DB
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW());"
+
+        data = {
+            "first_name": request.form["first_name"],
+            "last_name": request.form["last_name"],
+            "email": request.form["email"],
+            "password": pw_hash #STORES HASHED PASSWORD
+        }
+
+        users = connectToMySQL("login_reg_schema").query_db(query,data)
+        flash("Registration successful")
+
+    return redirect("/")
+
+@app.route('/login_user', methods=["POST"])
+def login_user():
+#VALIDATION
+
+    # see if the username provided exists in the database
+    # query the db
+    mysql = connectToMySQL("login_reg_schema")
+    query = "SELECT * FROM users WHERE email = %(email)s;"
+    data = { "email" : request.form["email_login"] }
+
+    #use the result variable to check the password, first make it a var
+    result = mysql.query_db(query, data)
+
+    #create var for the login password
+    pw_hash_login = request.form['password_login']
+    #check encrypted password
+    if len(result) > 0:
+        print(result[0]['password'])
+        print(pw_hash_login)
+        if bcrypt.check_password_hash(result[0]['password'], pw_hash_login):
+            #put userid in session
+            session['user_id'] = result[0]['id']
+            return redirect('/success')
+
+    flash("Invalid email or password, please try again")
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
